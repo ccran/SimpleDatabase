1d28ca408de1ea2fb1433bf0ab3ad45b5b470a84
ccran <i_chenran@cvte.com>
2019-11-20 20:03:14 +0800

    REPL

--- src/main/java/com/ccran/db/Main.java
+++ src/main/java/com/ccran/db/Main.java
@@ -1,9 +1,28 @@
 package com.ccran.db;
 
+import java.util.Scanner;
+
 /**
  * @author chenran
  * @description Main
  * @create 2019-11-20 19:51
  **/
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            printPrompt();
+            String input = scanner.nextLine().trim();
+            if (".exit".equals(input)) {
+                System.out.println("~");
+                return;
+            } else {
+                System.out.println(String.format("Unrecognized command '%s' .", input));
+            }
+        }
+    }
+
+    public static void printPrompt() {
+        System.out.print("db > ");
+    }
 }
