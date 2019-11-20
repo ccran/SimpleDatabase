72ea499968c290bf6365a95c5061ea2e5f0eb090
ccran <i_chenran@cvte.com>
2019-11-20 19:51:50 +0800

    项目创建

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java版本简单数据库
+原文C语言版：[Let’s Build a Simple Database](https://cstack.github.io/db_tutorial/)
+
---
+++ src/main/java/com/ccran/db/Main.java
@@ -0,0 +1,9 @@
+package com.ccran.db;
+
+/**
+ * @author chenran
+ * @description Main
+ * @create 2019-11-20 19:51
+ **/
+public class Main {
+}
